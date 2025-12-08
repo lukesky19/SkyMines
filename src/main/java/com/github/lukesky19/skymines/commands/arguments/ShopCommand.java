ac30c63cd6e78cbae22141216cdd3d486c3665b2
lukeskywlker19 <thelukeskywlker19@gmail.com>
2025-12-08 12:45:57 -0500

    Remove leftover debug message.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- src/main/java/com/github/lukesky19/skymines/commands/arguments/ShopCommand.java
+++ src/main/java/com/github/lukesky19/skymines/commands/arguments/ShopCommand.java
@@ -121,8 +121,6 @@ public class ShopCommand {
                     return 0;
                 }
 
-                System.out.println("Mine Id: " + mineId);
-
                 @Nullable WorldMineConfig mineConfig = mineConfigManager.getWorldMineConfig(mineId);
                 if(mineConfig == null) {
                     logger.warn(AdventureUtil.deserialize("The mine config for mine id " + mineId + " is invalid."));
